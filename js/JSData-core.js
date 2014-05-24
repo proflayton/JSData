4d04759761613c49b358aa91e3b9b8405c7b2259
proflayton <brandon0layton@yahoo.com>
2014-05-24 15:19:17 -0700

    ScatterGraph
    -Created the ScatterGraph object
    -Created the mouse handlers for the ScatterGraph object
    -Created getOffset(e). It's just a utility function
    -Made ScatterGraph's structure different, need to do the same to Pie
    -Changed some things in Pie to make it look cleaner
    -Split the data update function from the Draw function in ScatterGraph.
    Makes it look cleaner and allows for less computing when checking during
    the mouse events
    -Added fit() to the super. It allows users to call .fit() only on
    ScatterGraph objects

--- js/JSData-core.js
+++ js/JSData-core.js
@@ -9,6 +9,18 @@
 (function($){
 	
 	this.Data = []
+
+	//utility functions
+
+	//Takes borders into consideration
+	function getOffset(e){
+		var x = 0, y = 0;
+	    x += parseInt(e.offset().left);
+	    y += parseInt(e.offset().top) ;
+	    x += parseInt(e.css("border-left-width"));
+	    y += parseInt(e.css("border-top-width"));
+	    return { x: x, y: y };
+	}
 	
 	function Pie(ele){
 		var element = ele;
@@ -30,7 +42,7 @@
 			var x = event.pageX - element.offset().left,
 				y = event.pageY - element.offset().top ;
 			var pie = element.find(".data-pie-canvas");
-			var w = pie.attr("width"), h = pie.attr("height"),
+			var w = pie.width(), h = pie.height(),
 				r = (w > h ? w : h)/radiusFactor;
 			var total = 0,
 				points = [],
@@ -76,12 +88,13 @@
 		function Draw(){
 			var god = element;
 			var pie = god.find(".data-pie-canvas");
+			pie.width(god.width()); pie.height(god.height());
 			var piec= pie.get(0).getContext("2d");
 			var total = 0,
 				points = [],
 			    curAngle = 0,
 			    colors = [];
-			var w = pie.attr("width"), h = pie.attr("height"),
+			var w = pie.width(), h = pie.height(),
 				r = (w > h ? w : h)/radiusFactor;
 			var hoveredAngleStart = -1, hoveredAngleEnd = -1;
 			var hoveredIndex = parseInt(god.attr("data-hovered"),10);
@@ -155,53 +168,165 @@
 	};
 
 	//SCATTER GRAPH
-	function ScatterGraph(ele){
-		var element = ele;
-		element.prepend("<canvas class='data-scattergraph-canvas' width='"+element.width()+"'"+
-			" height='"+element.height()+"'></canvas>");
+	var ScatterGraph = function(ele){
+		//So we can call self from functions
+		var self = this;
+		this.element = ele;
+		this.element.prepend("<canvas class='data-scattergraph-canvas' width='"+this.element.width()+"'"+
+			" height='"+this.element.height()+"'></canvas>");
 
 		//Variable initializations
-		var pointSize = 2;//the diameter of each point
-		var pointHoverEnlargeFactor = 2; //2x the size when hovered
-		var zoomScaleX = 1.0, //variables for controlling the drawing of the graph
-			zoomScaleY = 1.0,
-			offsetX    = 0,
-			offsetY    = 0;
+		this.pointSize = 10;//the diameter of each point
+		this.pointHoverEnlargeFactor = 2; //2x the size when hovered
+		this.unitsPerX = 1.0; //variables for controlling the drawing of the graph
+		this.unitsPerY = 1.0;
+		this.offsetX    = 0;
+		this.offsetY    = 0;
+		this.dashesX    = 15;
+		this.dashesY    = 15;
 
-		function Draw(){
-			var god = element;
-			var graph = god.find(".data-scattergraph-canvas");
-			var cntxt = graph.get(0).getContext("2d");
-			var data = [];
+		this.colors = [];
+		this.dataPoints = [];
+		this.clickedData = -1;
+		this.minX = 0; this.maxX = 0;
+		this.minY = 0; this.maxY = 0;
+		//For some reason it wouldn't take it without it being in a subfunction.
+		//Should look into that later
+		this.element.mousemove(function(event){ self.handlemousemove(event); });
+		this.element.mousedown(function(event){ self.handlemousedown(event); });
+		this.element.mouseup(  function(event){ self.handlemouseup(event);   });
+
+		this.handlemousedown = function(event){
+			var god = self.element;
+			var o = getOffset(self.element);
+			var x = event.pageX - o.x,
+				y = event.pageY - o.y;
+			var w = self.element.width(),
+				h = self.element.height();
+			var r = self.pointSize/2;
+			//actual values on graph
+			var aX = self.unitsPerX * x     + self.offsetX,
+				aY = self.unitsPerY * (h-y) + self.offsetY;
+
+			for(var i = 0; i < self.dataPoints.length; i++){
+				var p = self.dataPoints[i];
+				var pX = (p[0] - self.offsetX)*(1/self.unitsPerX),
+					pY = h + (self.offsetY - p[1])*(1/self.unitsPerY);
+				
+				var dX = pX - x, dY = pY - y;
+				if(dX*dX + dY*dY < r*r){
+					self.clickedData = i;
+					return;
+				}
+
+			}
+		};
+		this.handlemouseup = function(event){ self.clickedData = -1;}
+		this.handlemousemove = function(event){
+			if(self.clickedData == -1) return; //If nothing clicked
+			console.log(self.clickedData);
+			var god = self.element;
+			var o = getOffset(self.element);
+			var x = event.pageX - o.x,
+				y = event.pageY - o.y;
+			var w = self.element.width(),
+				h = self.element.height();
+			//actual values on graph
+			var aX = self.unitsPerX * x     + self.offsetX,
+				aY = self.unitsPerY * (h-y) + self.offsetY;
+
+			var i = 0;
+			god.find(".data-point").each(function(){
+				if(i == self.clickedData){
+					$(this).attr("data-val",aX+","+aY);
+				}
+				i++;
+			});
+		};
+		this.update = function(){
+			var god = self.element;
 			var minX = null, maxX = null,
 				minY = null, maxY = null;
-
-			//Context initialization
-			cntxt.font = "10pt Arial";
-
-			//Get Data
-			god.find(".data-point").each(function(){
-				var valX = parseFloat($(this).attr("data-valx"),10);
-				var valY = parseFloat($(this).attr("data-valy"),10);
+			self.dataPoints = []; //clear data
+			self.colors = [];
+			god.find('.data-point').each(function(){
+				var val = $(this).attr("data-val");
+				var vals= val.split(",");
+				var valX = parseFloat(vals[0],10),
+					valY = parseFloat(vals[1],10);
 				//Initialize the min and max or set the new one
 				if(minX == null || valX < minX) minX = valX;
 				if(maxX == null || valX > maxX) maxX = valX;
 				if(minY == null || valY < minY) minY = valY;
 				if(maxY == null || valY > maxY) maxY = valY;
-				data.push([valX,valY]);
+
+				self.dataPoints.push([valX,valY]);
+				self.colors.push($(this).css("color"));
 			});
 
+			self.minX = minX; self.maxX = maxX;
+			self.minY = minY; self.maxY = maxY;
+		};
+		this.fit = function(){
+			self.update();
+			var god = self.element;
+			var w = god.width(),
+				h = god.height();
+
+			self.offsetX = self.minX;
+			self.offsetY = self.minY;
+
+			self.unitsPerX = (self.maxX - self.minX)/w;
+			self.unitsPerY = (self.maxY - self.minY)/h;
+
+			console.log(self.unitsPerX + "," + self.unitsPerY);
+		};
+		this.Draw = function(){
+			self.update();
+			var god = self.element;
+			var graph = god.find(".data-scattergraph-canvas");
+			graph.width(god.width()); graph.height(god.height());
+			var cntxt = graph.get(0).getContext("2d");
+			var w = graph.width(),
+				h = graph.height();
+			var r = self.pointSize/2;
+
+			//Context initialization
+			cntxt.font = "10pt Arial";
+			cntxt.imageSmoothingEnabled = true;
+			cntxt.lineCap     = "round";
+			cntxt.strokeStyle = "#222";
+			cntxt.lineWidth   = 2;
+			cntxt.clearRect(0,0,w,h); //clear canvas
+
 			//the inbetween point of the min and max, then the zoom scale
-			var midX = (1/zoomScaleX)*(minX+maxX)/2;
-			var midY = (1/zoomScaleY)*(minY+maxY)/2;
-
+			var xO = self.offsetX * (1/self.unitsPerX),
+				yO = self.offsetY * (1/self.unitsPerY);
+			//console.log(unitsPerX+","+unitsPerY);
+			var y = h + yO;
+			var x = -xO;
 			cntxt.beginPath();
-		}
+			cntxt.moveTo(x,0); cntxt.lineTo(x,h);
+			cntxt.stroke();
+			cntxt.beginPath();
+			cntxt.moveTo(0,y); cntxt.lineTo(w,y);
+			cntxt.stroke();
 
+			for(var i in self.dataPoints){
+				var p = self.dataPoints[i];
+				//console.log(r);
+				cntxt.fillStyle = self.colors[i];
+				cntxt.beginPath();
+				cntxt.arc(x + p[0]*(1/self.unitsPerX), y - p[1]*(1/self.unitsPerY),r,0,Math.PI*2);
+				cntxt.fill();
+			}
+		};
 
-		Draw(); //initially draw the graph
-		UpdateInterval = setInterval(Draw, 25);
+		this.fit();
+		this.Draw();
+		this.UpdateInterval = setInterval(this.Draw,25);
 	};
+	
 
 	//function to create the pie initializer in jQuery
 	$.fn.pie = function(){
@@ -211,6 +336,26 @@
 	//function to create the scatter graph initializer in jQuery
 	$.fn.scattergraph = function(){
 		Data.push(new ScatterGraph(this));
+		return this;
+	};
+
+	$.fn.fit = function(){
+		//If the type of object doesn't have this function,
+		if(!(
+			this.attr("class") == "data-scattergraph"
+			))
+		{
+			console.log("not right data type to call fit()");
+			return this;
+		}
+		for(var d in Data){
+			if(Data[d].element == this){ //find the corresponding Data object
+				console.log(Data[d]);
+				Data[d].fit();
+				return this;
+			}
+		}
+
 		return this;
 	};
 	
@@ -226,7 +371,7 @@
 				if(!val) val = 0;
 				god.attr("data-val",val);
 			};
-			
+
 		var element   = arguments.length == 1 ? arguments[0] : arguments[1];
 		var eventData = arguments.length == 1 ? "change" : arguments[0];
 
